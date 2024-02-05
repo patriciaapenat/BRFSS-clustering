0ec120d0dd106da37b2d6396cdf42655d8161223
Patricia Peña <111457653+patriciaapenat@users.noreply.github.com>
2024-02-05 18:33:34 +0100

    avances

--- Notebooks/import os.py
+++
@@ -1,9 +0,0 @@
-import os
-from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
-
-
-# Configurar SPARK_HOME y añadir el directorio bin al PATH
-spark_home = r"C:\Python312\Scripts"
-os.environ["SPARK_HOME"] = spark_home
-os.environ["PATH"] += os.pathsep + os.path.join(spark_home, "bin")
